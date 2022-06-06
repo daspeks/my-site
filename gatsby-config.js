de4af565cf1d7e8ce74fbb16b0861236d06d983d
Parth <daspeks@gmail.com>
2022-06-06 12:48:05 -0600

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,15 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.daspeks.gatsbyjs.io`,
   },
-  plugins: ["gatsby-plugin-image",
-  "gatsby-plugin-sharp",],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 };
